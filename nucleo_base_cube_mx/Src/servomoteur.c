b39f93217437ecf6087a75280a51364515a1bf8f
Laura Bouzereau <bouzerea@insat.insa-toulouse.fr>
2018-11-12 13:44:37 +0100

    ajout arguments min et max dans servomoteur

--- nucleo_base_cube_mx/Src/servomoteur.c
+++ nucleo_base_cube_mx/Src/servomoteur.c
@@ -1,12 +1,8 @@
 #include "servomoteur.h"
-float pulse;
-float tick;
-float duree_pulse;
+//int max=1950; //1472   pour le bras
+//int min=450; //352   pour le bras
 
-int max=1472;
-int min=352;
-
-void set_angle(float angle, TIM_HandleTypeDef *timer,uint32_t channel){
+void set_angle(float angle, TIM_HandleTypeDef *timer,uint32_t channel, int min, int max){
 	//tick = (float)(timer->Init.Prescaler) / 72000000.;
 	//duree_pulse = (1. + angle /100. )/1000.;
 	//pulse = (duree_pulse / tick);
@@ -16,9 +12,8 @@ void set_angle(float angle, TIM_HandleTypeDef *timer,uint32_t channel){
 	
 	 //pulse = ((angle*(2)) * 72*1000000) /(timer->Init.Period * timer->Init.Prescaler); //comparison = angle%age * periode 
 	
-	pulse = ((max-min) * (angle / 100)) + min;
+	int pulsemax = ((max-min) * (angle / 100)) + min;
 	
-	__HAL_TIM_SET_COMPARE(timer,channel ,pulse);
-	
+	__HAL_TIM_SET_COMPARE(timer,channel ,pulsemax);
 }
 	
